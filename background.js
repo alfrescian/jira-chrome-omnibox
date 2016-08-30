b159739020012dc29afc272d63a6013914bbf99e
Jan Pfitzner <jan.pfitzner@westernacher.com>
2016-08-30 20:23:24 +0200

    code cleanup

--- background.js
+++ background.js
@@ -1,46 +1,44 @@
+const
+  baseUrl = 'https://jira.westernacher.com',
+  keyPrefix = 'NSWDEV';
+
 function resetDefaultSuggestion() {
     chrome.omnibox.setDefaultSuggestion({
-        description: 'open JIRA ticket: NSWDEV-%s'
+        description: `open JIRA ticket: ${keyPrefix}-%s`
     });
 }
 
 resetDefaultSuggestion();
 
 chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
-    fetch('https://jira.westernacher.com/rest/api/2/search?jql=key=NSWDEV-' + text, {
+    fetch(`${baseUrl}/rest/api/2/search?jql=key=${keyPrefix}-${text}`, {
         credentials: 'include'
     })
-    .then(function (res) {
-        return res.json()
-    })
-    .then(function (result) {
+    .then(res => res.json())
+    .then(result => {
         if (result && result.issues && result.issues.length > 0) {
           const issue = result.issues[0];
           chrome.omnibox.setDefaultSuggestion(
-              {description: `<match>${issue.fields.summary}</match> - <url>https://jira.westernacher.com/browse/NSWDEV-${text}</url>`}
+              {description: `<match>${issue.fields.summary}</match> - <url>${baseUrl}/browse/${keyPrefix}-${text}</url>`}
           );
         }
         else {
             resetDefaultSuggestion();
         }
     })
-    .catch(function (error) {
+    .catch(error => {
         console.error(error);
         resetDefaultSuggestion();
     });
 });
 
-chrome.omnibox.onInputCancelled.addListener(function () {
-    resetDefaultSuggestion();
-});
+chrome.omnibox.onInputCancelled.addListener(() => resetDefaultSuggestion());
 
 
 function navigate(url) {
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
         chrome.tabs.update(tabs[0].id, {url: url});
     });
 }
 
-chrome.omnibox.onInputEntered.addListener(function (ticketNumber) {
-    navigate("https://jira.westernacher.com/browse/NSWDEV-" + ticketNumber);
-});
+chrome.omnibox.onInputEntered.addListener(ticketNumber => navigate(`${baseUrl}/browse/${keyPrefix}-${ticketNumber}`));
